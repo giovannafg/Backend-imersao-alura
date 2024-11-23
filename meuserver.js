7990facb8347324562fa4dc19bbf8747b92cc1ee
giovannafg <giovannafelp@gmail.com>
2024-11-23 00:26:46 -0300

    aula 3

--- meuserver.js
+++ meuserver.js
@@ -1,31 +1,10 @@
 import express from "express";
-
-// array apaga os dados depois, logo: tem q usar um banco:  Mongo
-
-const posts = [
-    { id: 1, descricao: "Gato um", imagem: "https://placecats.com/millie/300/150" },
-    { id: 2, descricao: "Gato dois", imagem: "https://placecats.com/millie/300/150" },
-    { id: 3, descricao: "Gato tres", imagem: "https://placecats.com/millie/300/150"},
-];
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-app.use(express.json());
+
+routes(app);
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
-});
-
-app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
-});
-
-function buscarpostporID(id){
-    return posts.findIndex((post) => {
-        return post.id === Number(id)
-    })
-}
-
-app.get("/posts/:id", (req, res) => {
-    const index = buscarpostporID(req.params.id)
-    res.status(200).json(posts[index]);
 });
