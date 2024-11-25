ed6036c4a14dbf16f47454ba07df316b1d3d9881
giovannafg <giovannafelp@gmail.com>
2024-11-25 18:00:47 -0300

    deploy do projeto

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,3 +1,5 @@
+import 'dotenv/config'
+import { ObjectId } from "mongodb";
 import conectarAoBanco from "../config/dbConfig.js"
 // Conecta ao banco de dados utilizando a string de conexão fornecida como variável de ambiente
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
@@ -16,4 +18,11 @@ export async function criarPost(novoPost) {
     const db = conexao.db("instabytes");
     const colecao = db.collection("posts");
     return colecao.insertOne(novoPost)
+}
+
+export async function atualizarPost(id, novoPost) {
+    const db = conexao.db("instabytes");
+    const colecao = db.collection("posts");
+    const objID = ObjectId.createFromHexString(id);
+    return colecao.updateOne({_id: new ObjectId(objID)}, {$set:novoPost});
 }
